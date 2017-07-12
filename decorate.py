2acbe173e6d87d4722a4065535572c1f067a31a1
jinpin.hong <329794129@qq.com>
2017-07-12 15:48:36 +0800

    save

---
+++ decorate.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import logging
+import traceback
+import sys
+from functools import wraps
+from django.utils.decorators import available_attrs
+from django.http.response import HttpResponse
+
+view_log = logging.getLogger('view')
+
+def view_exception_handler(view_func):
+    """
+    view异常处理
+    """
+
+    def wrapped_view(*args, **kwargs):
+
+        try:
+            return view_func(*args, **kwargs)
+        except Exception:
+            view_log.info(traceback.format_exc())
+            return HttpResponse('SUCCESS')
+
+    return wraps(view_func, assigned=available_attrs(view_func))(wrapped_view)
