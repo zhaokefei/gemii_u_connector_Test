4f01ad4c91094e4f17de382ed2ac075c5dfd3e4a
jinpin.hong <329794129@qq.com>
2017-07-25 17:44:04 +0800

    fix up memberinfo

--- connector/views.py
+++ connector/views.py
@@ -479,8 +479,6 @@ class MemberInfoCreateView(GenericAPIView, mixins.CreateModelMixin):
                 instance = MemberInfo.objects.filter(vcSerialNo=member['vcSerialNo'])
                 if member['dtCreateDate']:
                     member['dtCreateDate'] = commont_tool.time_strf(member['dtCreateDate'])
-                else:
-                    member.pop('dtCreateDate')
                 if member['dtLastMsgDate']:
                     member['dtLastMsgDate'] = commont_tool.time_strf(member['dtLastMsgDate'])
                 else:
