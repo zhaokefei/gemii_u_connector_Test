70e423b4ee30769189be9c5ec82d3672b2a811ab
jingtao <jt@jtdeMacBook-Pro.local>
2017-10-19 15:48:45 +0800

    未获取机器人二维码-测试服务器

--- connector/utils/commont_tool.py
+++ connector/utils/commont_tool.py
@@ -11,6 +11,7 @@ from connector import apis
 import base64
 
 from django.conf import settings
+from connector.models import ChatRoomModel
 
 
 message_log = logging.getLogger('message')
@@ -79,13 +80,25 @@ def emoji_to_unicode(nickname):
         result = nickname
     return result
 
-
+# 获取 小助手二维码失败 发送消息
 def get_robotQucode_fail(post, u_response):
-    message = u"群二维码未获得,请查看：\n群名称:%s \ntask_id:%s \n错误信息为:%s" \
-              % (post[u'group_name'], post[u'task_id'],  u_response[u'msg'])
-    response = apis.send_chat_message(vcChatRoomSerialNo='5E95FD246CF899AA2AFF029022F75217',
-                                      vcRobotSerialNo='C6C612A0F755EA449275DD9C3E057F54',
+    # 如果没有 群名称 在数据库中查询
+    if str(post['group_name']) == '':
+        try:
+            conn_chatroom = ChatRoomModel.objects.get(vcChatRoomSerialNo=post['chat_room_id'])
+            post['group_name'] = conn_chatroom.vcName
+        except:
+            post['group_name'] = post['chat_room_id']
+
+    message = "小助手二维码未获得,请查看：\n群名称:%s \ntask_id:%s \n错误信息为:%s" \
+              % (post['group_name'], post['task_id'],  u_response['msg'])
+
+
+    # response = apis.send_chat_message(vcChatRoomSerialNo='5E95FD246CF899AA2AFF029022F75217',
+    #                                   vcRobotSerialNo='C6C612A0F755EA449275DD9C3E057F54',
+    response = apis.send_chat_message(vcChatRoomSerialNo='4FCB1F0E5C1136ADBFCE013648023C9E',
+                                      vcRobotSerialNo='FDBFB2B1EE75CA333AADE7D934F8468D',
                                       vcWeixinSerialNo='',
                                       msgContent=message)
 
-    message_log.info('robotQucode_fail message response %s' % str(response))
+    message_log.info('robotQucode_fail message response: %s' % str(response))
